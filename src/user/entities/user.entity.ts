36875b0ead1d601e596478cbccf8684121c584f0
Eduardo Saes <eduardodev08@gmail.com>
2021-09-04 12:57:19 -0400

    Added entity user

---
+++ src/user/entities/user.entity.ts
@@ -0,0 +1,48 @@
+import {
+  PrimaryGeneratedColumn,
+  Column,
+  CreateDateColumn,
+  BeforeInsert,
+  BeforeUpdate,
+  Entity,
+} from 'typeorm';
+import { hash } from 'bcryptjs';
+
+@Entity('users')
+export class User {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ type: 'varchar', length: 255, default: '', nullable: true })
+  name: string;
+
+  @Column({
+    name: 'last_name',
+    type: 'varchar',
+    length: 255,
+    default: '',
+    nullable: true,
+  })
+  lastName: string;
+
+  @Column({ type: 'varchar', length: 255, nullable: false })
+  email: string;
+
+  @Column({ type: 'varchar', length: 128, nullable: false, select: false })
+  password: string;
+
+  @Column({ type: 'varchar', nullable: false })
+  role: string;
+
+  @CreateDateColumn({ name: 'created_at', type: 'timestamp' })
+  createdAt: Date;
+
+  @BeforeInsert()
+  @BeforeUpdate()
+  async hashPassword() {
+    if (!this.password) {
+      return;
+    }
+    this.password = await hash(this.password, 10);
+  }
+}
